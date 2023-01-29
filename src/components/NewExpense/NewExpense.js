e1369e6fbe728ae9fa7dcec4097e1e661cb1448f
Yogesh Patel <yogi.patel@junction.ai>
2023-01-28 19:44:33 -0500

    child to parent state

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import "./NewExpense.css";
-
-import ExpenseForm from './ExpenseForm';
-
 // we can pass data from child to parent by utilizing props as function
 // in parent component and call the function in child component
 
-const NewExpense = () => {
-    return(
-        <div className='new-expense'>
-            <ExpenseForm />
-        </div>
-    )
+import React from 'react';
+
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+    props.onAddExpense(expenseData);
+  };
+
+  return (
+    <div className='new-expense'>
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
 };
 
 export default NewExpense;
